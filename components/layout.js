326c5195c40133e18ae2faf28f1406ce379792fb
jsahlsa <joe.sahlstrom@gmail.com>
2022-11-16 21:00:35 -0800

    ✨ added og image, meta info

--- components/layout.js
+++ components/layout.js
@@ -15,8 +15,14 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="Andy's site" />
         <meta property="og:url" content="https://andysahlstrom.com" />
-        <meta property="og:image" content="/andy-og.png" />
-        <meta property="og:image:secure_url" content="/andy-og.png" />
+        <meta
+          property="og:image"
+          content="https://andysahlstrom.com/andy-og.png"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://andysahlstrom.com/andy-og.png"
+        />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@shampooty" />
@@ -25,7 +31,10 @@ export default function Layout({ children }) {
         <meta property="twitter:url" content="https://andysahlstrom.com" />
         <meta name="twitter:title" content="Andy Sahlstrom" />
         <meta name="twitter:description" content="Andy Sahlstrom's Portfolio" />
-        <meta name="twitter:image:src" content="/andy-og.png" />
+        <meta
+          name="twitter:image:src"
+          content="https://andysahlstrom.com/andy-og.png"
+        />
         <link rel="icon" sizes="any" type="image/svg+xml" href="/logo_a.svg" />
         <link
           rel="apple-touch-icon"
