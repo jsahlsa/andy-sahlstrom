f8931f7739c4279277a0b5ff301c52483a5c38af
jsahlsa <joe.sahlstrom@gmail.com>
2022-12-05 19:43:01 -0800

    ✨ added kids toys adult issues, and art show splash on main page

--- pages/index.js
+++ pages/index.js
@@ -11,29 +11,29 @@ export default function Home() {
   return (
     <Layout>
       <Nav />
-      <div
-        className={adClose ? styles.ad_container : styles.ad_container_closed}
-      >
-        <Link href="/design/kids-toys-adult-issues">
-          <div className={styles.ad_image_container}>
-            <Image
-              src="/images/kids-toys-show.jpeg"
-              width={2341}
-              height={2240}
-              objectFit="cover"
-              layout="responsive"
-            />
-          </div>
-        </Link>
-        <span onClick={() => setAdClose(false)} className={styles.close_box}>
-          <svg className={styles.close_box_svg} width="30" height="30">
-            <line className={styles.line_one} x1="0" y1="0" x2="18" y2="18" />
-            <line className={styles.line_one} x1="0" y1="18" x2="18" y2="0" />
-          </svg>
-        </span>
-      </div>
 
       <main className={styles.main_container}>
+        <div
+          className={adClose ? styles.ad_container : styles.ad_container_closed}
+        >
+          <Link href="/design/kids-toys-adult-issues">
+            <div className={styles.ad_image_container}>
+              <Image
+                src="/images/kids-toys-show.jpeg"
+                width={2341}
+                height={2240}
+                objectFit="cover"
+                layout="responsive"
+              />
+            </div>
+          </Link>
+          <span onClick={() => setAdClose(false)} className={styles.close_box}>
+            <svg className={styles.close_box_svg} width="30" height="30">
+              <line className={styles.line_one} x1="0" y1="0" x2="18" y2="18" />
+              <line className={styles.line_one} x1="0" y1="18" x2="18" y2="0" />
+            </svg>
+          </span>
+        </div>
         <div
           className={styles.main_video_container}
           style={{
